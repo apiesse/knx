fab20d09db0c0c1377d591febe844397d76978bd
Mirco Pattera <mirco.pattera@gmail.com>
2026-09-16 10:46:54 +0200

    fix(build): avoid lwIP macros before Arduino IPAddress declarations

--- src/knx/bits.h
+++ src/knx/bits.h
@@ -5,7 +5,7 @@
 
 #if defined(__linux__)
 #include <arpa/inet.h>
-#elif defined(ESP_PLATFORM)
+#elif defined(ESP_PLATFORM) && !defined(ARDUINO)
 #include <lwip/inet.h>
 #elif defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_STM32) || defined (DeviceFamily_CC13X0)
 #define getbyte(x,n) (*(((uint8_t*)&(x))+n))
